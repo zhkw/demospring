bc7b655325ac821a3a09fe92b3fc8b4cb112d34e
zhkw <13910066913@163.com>
2020-05-06 16:26:55 +0800

    first commit

---
+++ src/main/java/com/zkw/demospring/DemospringApplication.java
@@ -0,0 +1,14 @@
+package com.zkw.demospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemospringApplication.class, args);
+    }
+
+}
